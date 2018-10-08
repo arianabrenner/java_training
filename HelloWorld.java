19bab8ad002818f3b2f3638b0003a9deb01d4a61
arianabrenner <arianaebrenner@gmail.com>
2018-10-07 21:39:38 -0400

    Add hello world code

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/* Name : Ariana Brenner 
+ * PennKey : arianabr
+ * Recitation : ???
+ * 
+ * Execution: java HelloWorld
+ * 
+ * Prints: "Hello, World."
+ * 
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+    }
+}
